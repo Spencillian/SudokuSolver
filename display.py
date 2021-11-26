b2e6c763add3c5fdcc3d364261bffbb9c2d52949
MovoLovo <48698410+Spencillian@users.noreply.github.com>
2021-11-26 03:56:10 -0500

    Update display.py

--- display.py
+++ display.py
@@ -1,6 +1,20 @@
 import tkinter as tk
 
 
+def get_input(inputs: list) -> list:
+    result = []
+    for row in inputs:
+        temp = []
+        for entry in row:
+            if entry.get():
+                temp.append(int(entry.get()))
+            else:
+                temp.append(None)
+        result.append(temp)
+    print(result)
+    return result
+
+
 def draw(board: list) -> None:
     root = tk.Tk()
     inputs = []
@@ -8,12 +22,12 @@ def draw(board: list) -> None:
     for i in range(len(board)):
         temp = []
         for j in range(len(board)):
-            e = tk.Entry(root, width=2)
+            e = tk.Entry(root, width=2, borderwidth=10)
             temp.append(e)
             e.grid(row=i, column=j)
         inputs.append(temp)
 
-    tk.mainloop()
+    btn = tk.Button(root, text="Input", command=lambda: get_input(inputs))
+    btn.grid(row=10, column=0)
 
-# TODO: add function for getting inputs
-# TODO: add button for access to function
+    tk.mainloop()
